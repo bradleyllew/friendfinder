d7306ac8dd70bb09a2ce1658cb91191ec1a7fadc
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-10-16 17:13:55 -0400

    Trying to push to Heroku

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routing/apiRoutes.js")(app);
-require("./routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
