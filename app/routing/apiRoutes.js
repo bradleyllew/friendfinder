a967def372211e13686d24368978cd40803b6351
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-10-21 18:28:04 -0400

    test

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -22,55 +22,55 @@ module.exports = function (app) {
         };
 
         // this is to take the score and convert to string
-    var userData = req.body;
-    var userName = userData.name;
-    var userScores = userData.scores;
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+        console.log(userScores)
+        var b = userScores.map(function (item) {
+            return parseInt(item, 10);
+        });
 
-    var b = userScores.map(function(item) {
-        return parseInt(item, 10);
-    });
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: b
+        };
 
-    userData = {
-        name: req.body.name,
-        photo: req.body.photo,
-        scores: b
-    };
+        console.log("User Name: " + userName);
+        console.log("User Scores: " + userScores);
 
-    console.log("User Name: " + userName);
-    console.log("User Scores: " + userScores);
-
-    var sum = b.reduce((a, b) => a + b, 0)
-    console.log("User Total Score: " + sum);
-    console.log("Best Match Freind Difference " + bestMatch.friendDifference);
-
-    // loops through to find differences in scores for each friend
-    for(var i = 0; i < friends.length; i++) {
-        console.log(friends[i].name);
-        totalDifference = 0;
-        console.log("Total Difference " + totalDifference);
+        var sum = b.reduce((a, b) => a + b, 0)
+        console.log("User Total Score: " + sum);
         console.log("Best Match Freind Difference " + bestMatch.friendDifference);
-   
-        var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
-        console.log("Total Score " + bfriendScore);
-        totalDifference += Math.abs(sum - bfriendScore);
-        console.log("Total Difference is " + totalDifference);
-    
-    // determines the best match for friend
-        if (totalDifference <= bestMatch.friendDifference) {
-            bestMatch.name = friends[i].name;
-            bestMatch.photo = friends[i].photo;
-            bestMatch.friendDifference = totalDifference;
+
+        // loops through to find differences in scores for each friend
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i].name);
+            totalDifference = 0;
+            console.log("Total Difference " + totalDifference);
+            console.log("Best Match Freind Difference " + bestMatch.friendDifference);
+
+            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+            console.log("Total Score " + bfriendScore);
+            totalDifference += Math.abs(sum - bfriendScore);
+            console.log("Total Difference is " + totalDifference);
+
+            // determines the best match for friend
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+
+            console.log("Total Difference is " + totalDifference);
         }
 
-        console.log("Total Difference is " + totalDifference);
-     }
-
-     console.log(bestMatch);
-    //  to add to the array
-     friends.push(userData);
-     console.log("New User Added");
-     console.log(userData);
-     res.json(bestMatch);
+        console.log(bestMatch);
+        //  to add to the array
+        friends.push(userData);
+        console.log("New User Added");
+        console.log(userData);
+        res.json(bestMatch);
     });
 
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "./app/public")));
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
